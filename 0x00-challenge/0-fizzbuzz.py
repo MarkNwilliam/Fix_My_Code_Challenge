ebcb6eb1212741ff6b0f4b84a3265476785a60a8
MarkNWiiam <nkugwamarkwiliam@gmail.com>
2023-03-27 01:22:18 +0300

    Signed-off-by: MarkNWiiam <nkugwamarkwiliam@gmail.com>

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 def fizzbuzz(n):
     """
     FizzBuzz function prints numbers from 1 to n separated by a space.
@@ -19,3 +21,16 @@ def fizzbuzz(n):
         else:
             tmp_result.append(str(i))
     print(" ".join(tmp_result))
+
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) <= 1:
+        print("Missing number")
+        print("Usage: ./0-fizzbuzz.py <number>")
+        print("Example: ./0-fizzbuzz.py 89")
+        sys.exit(1)
+
+    number = int(sys.argv[1])
+    fizzbuzz(number)
